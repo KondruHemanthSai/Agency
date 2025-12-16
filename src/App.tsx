94924d578a2784091ed270758d2a157733305370
KondruHemanthSai <kondru.hemanthsai@gmail.com>
2025-12-16 15:17:29 +0530

    fix: Adjust background to black/grey mix and reduce speed

--- src/App.tsx
+++ src/App.tsx
@@ -24,11 +24,12 @@ const App = () => (
       <TooltipProvider>
         <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: -1, pointerEvents: 'none' }}>
           <LiquidChrome
-            baseColor={[0.1, 0.1, 0.4]}
-            speed={1}
-            amplitude={0.4}
+            baseColor={[0.1, 0.1, 0.1]}
+            speed={0.2}
+            amplitude={0.3}
             interactive={false}
           />
+          <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(0,0,0,0.3)' }} />
         </div>
         <Loader />
         <Toaster />
